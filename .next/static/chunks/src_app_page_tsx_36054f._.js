bdbe356d85d71f0fc535ea51b4ab433530344a05
gts.senna <gts.senna@gmail.com>
2024-12-20 22:25:45 -0300

    build

--- .next/static/chunks/src_app_page_tsx_36054f._.js
+++ .next/static/chunks/src_app_page_tsx_36054f._.js
@@ -5,8 +5,17 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_36054f._.js",
   "chunks": [
-    "static/chunks/node_modules_3803f9._.js",
-    "static/chunks/_0b95a4._.js"
+    "static/chunks/node_modules_next_8182fa._.js",
+    "static/chunks/node_modules_flowbite-react_dist_esm_83f8f5._.js",
+    "static/chunks/node_modules_react-icons_hi_index_mjs_d23786._.js",
+    "static/chunks/node_modules_react-icons_fa6_index_mjs_00e856._.js",
+    "static/chunks/node_modules_react-icons_md_index_mjs_78df2b._.js",
+    "static/chunks/node_modules_react-icons_fa_index_mjs_d2e2d7._.js",
+    "static/chunks/node_modules_react-icons_lib_74ccc9._.js",
+    "static/chunks/node_modules_@floating-ui_react_dist_c1a64d._.js",
+    "static/chunks/node_modules_axios_lib_c4c49c._.js",
+    "static/chunks/node_modules_3a1ef9._.js",
+    "static/chunks/_8e99d4._.js"
   ],
   "source": "dynamic"
 });
